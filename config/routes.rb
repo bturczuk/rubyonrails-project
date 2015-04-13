b1cd4ef68c9072b6b9cd5bbda4218c237a0c6613
Beata Turczuk <b.turczukk@gmail.com>
2015-04-13 19:23:18 +0200

    users crud is correct

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
   get 'users/new'
 
     root 'static_pages#home'
@@ -10,6 +12,11 @@ Rails.application.routes.draw do
     
   get 'contact' => 'static_pages#help'
       get 'signup'  => 'users#new'
+      get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+    
+    resources :users
 
 
   # The priority is based upon order of creation: first created -> highest priority.
